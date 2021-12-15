ceda14e9de134730d1b0646d3116f58e3baa4922
root <root@DESKTOP-6M2A902.localdomain>
2021-12-15 21:25:43 +0700

    first commit

--- .history/index_20211215212116.js
+++ .history/index_20211215212116.js
@@ -1,15 +1,14 @@
 import express from "express";
-import * as functions from 'firebase-functions'
+import cors from 'cors'
 
 import usersRoutes from "./Routes/users.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use("/api", usersRoutes);
+app.use(cors)
 app.get("/", (req, res) => res.send("Welcome to the Users API!"));
 app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
 
 app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
-
-exports.api = functions.https.onRequest(api)
