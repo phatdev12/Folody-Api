9e7e4ff881ff12b9f5a94d88ce37a456a37bca8a
Phatdev <tuphat3456tt@gmail.com>
2021-12-15 11:30:44 +0700

    updates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import usersRoutes from "./Routes/users.js";
 
 const app = express();
-const PORT = 3001 || process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 
